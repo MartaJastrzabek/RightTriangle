f1e1d53a8dc52be72cb5971a0a83efeb04db6a7a
Marta <marta.jastrzabek@gmail.com>
2019-07-15 13:57:57 +0200

    Updated version of checking if triangle is right.

--- src/RightTriangle.java
+++ src/RightTriangle.java
@@ -1,7 +1,12 @@
 public class RightTriangle {
 
     boolean isRightTriangle(Triangle triangle){
-        boolean isRight = ((triangle.a * triangle.a) + (triangle.b * triangle.b)) == (triangle.c * triangle.c);
+        boolean isRightA = ((triangle.b * triangle.b) + (triangle.c * triangle.c)) == (triangle.a * triangle.a);
+        boolean isRightB = ((triangle.a * triangle.a) + (triangle.c * triangle.c)) == (triangle.b * triangle.b);
+        boolean isRightC = ((triangle.a * triangle.a) + (triangle.b * triangle.b)) == (triangle.c * triangle.c);
+
+        boolean isRight = isRightA || isRightB || isRightC;
+
         return isRight;
     }
 }
--- src/RightTriangleTest.java
+++ src/RightTriangleTest.java
@@ -8,10 +8,25 @@ public class RightTriangleTest {
         System.out.println("Triangle is right: " + isRight);
         System.out.println();
 
-        Triangle triangle2 = new Triangle(3, 4 , 5);
+        Triangle triangle2 = new Triangle(3,4,5);
         isRight = triangleTest.isRightTriangle(triangle2);
         System.out.println("Triangle is right: " + isRight);
+        System.out.println();
 
+        Triangle triangle3 = new Triangle(5,3,4);
+        isRight = triangleTest.isRightTriangle(triangle2);
+        System.out.println("Triangle is right: " + isRight);
+        System.out.println();
+
+        Triangle triangle4 = new Triangle(4,3,5);
+        isRight = triangleTest.isRightTriangle(triangle4);
+        System.out.println("Triangle is right: " + isRight);
+        System.out.println();
+
+        Triangle triangle5 = new Triangle(4,5,3);
+        isRight = triangleTest.isRightTriangle(triangle5);
+        System.out.println("Triangle is right: " + isRight);
+        System.out.println();
 
 
     }
